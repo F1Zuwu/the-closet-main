cc035433406d02739efac53c5607d64c7f450a32
F1Zuwu <113283931+F1Zuwu@users.noreply.github.com>
2025-04-02 13:39:12 +0300

    login + register

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+
+// CUSTOM + TAILWINDCSS
 import './index.css';
 
-//containers
+// CONTIANERS
 import Home from './container/Home';
 import Add from './container/Add';
+import Login from './container/Login';
+import Register from './container/Reigster';
 
 ReactDOM.render(
     <BrowserRouter>
         <Routes>
+            {/* MAIN */}
             <Route path='/' element={<Home />} />
             <Route path='/add' element={<Add />} />
+
+            {/* AUTH */}
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
         </Routes>
     </BrowserRouter>,
     document.getElementById('root'),
