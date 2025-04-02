41afcd73be5f1e4eb24243e60faccfabaa6a1ac5
F1Zuwu <113283931+F1Zuwu@users.noreply.github.com>
2025-04-02 13:52:24 +0300

    cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const TestRouter = require("./routes/TestRouter");
 const userRouter = require("./routes/userRouter");
+const corsHandler = require('./middleware/cors');
 const app = express();
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(corsHandler)
 
 app.use("/api", TestRouter);
 app.use("/api", userRouter);
