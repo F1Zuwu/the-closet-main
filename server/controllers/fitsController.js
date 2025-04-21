86f694fbfba7ee97a1bd660bf91197feb9c29638
Kri273 <kristelle.tasane@voco.ee>
2025-04-21 13:34:45 +0300

    id problem fixed

--- server/controllers/fitsController.js
+++ server/controllers/fitsController.js
@@ -12,37 +12,40 @@ class fitsController extends BaseController {
 
   async addFit(req, res) {
     this.handleRequest(req, res, async () => {
-      const { name, clothing_ids, accessory_ids, image_url, tag_ids, user_id } =
+      const { name, clothing_ids, accessory_ids, image_url, tag_ids } =
         req.body;
-  
-      if (!clothing_ids || !Array.isArray(clothing_ids) || clothing_ids.length === 0) {
+
+      if (
+        !clothing_ids ||
+        !Array.isArray(clothing_ids) ||
+        clothing_ids.length === 0
+      ) {
         return res.status(400).json({
           success: false,
           message: "At least one clothing_id is required",
         });
       }
-  
+
       if (!tag_ids || !Array.isArray(tag_ids) || tag_ids.length === 0) {
         return res.status(400).json({
           success: false,
           message: "At least one tag_id is required",
         });
       }
-  
+
       try {
         const fit = await models.fits.create({
           name,
           image_url,
-          user_id,
+          user_id: req.user.id,
         });
         await fit.addClothing(clothing_ids);
         await fit.addTags(tag_ids);
         await fit.addAccessories(accessory_ids);
-        
-        console.log("Added clothing IDs:", clothing_ids);
-      console.log("Added tag IDs:", tag_ids);
-    console.log("Added accessory IDs:", accessory_ids);
 
+        console.log("Added clothing IDs:", clothing_ids);
+        console.log("Added tag IDs:", tag_ids);
+        console.log("Added accessory IDs:", accessory_ids);
 
         console.log("Fit created with clothing, tags, and accessories:", fit);
 
@@ -112,14 +115,14 @@ class fitsController extends BaseController {
             { model: models.accessory, through: { attributes: [] } },
           ],
         });
-  
+
         if (!fit) {
           return res.status(404).json({
             success: false,
             message: "Fit not found",
           });
         }
-  
+
         return res.status(200).json({
           success: true,
           fit,
